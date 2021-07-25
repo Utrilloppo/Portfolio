f3aa59eb34653d73b25baac2874c5dec61b76f69
Urillo <hohuraman@icloud.com>
2021-07-25 18:18:23 +0900

    add TimeStamp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/utils.dart';
 
 void main() {
   runApp(MyApp());
@@ -50,24 +51,31 @@ class _MyHomePageState extends State<MyHomePage> {
     new ThreadPostData(
         userName: "userTest1",
         userThumbnail: "",
-        postTimeStamp: 202000,
+        postTimeStamp: DateTime.now()
+            .subtract(new Duration(hours: 2))
+            .millisecondsSinceEpoch,
         postContent: "This is Content",
         postCommentCount: 2,
         postLikeCount: 4,
         postImage: ''),
     new ThreadPostData(
-        userName: "userTest1",
+        userName: "Miyu",
         userThumbnail: "",
-        postTimeStamp: 202000,
-        postContent: "This is Content",
+        postTimeStamp: DateTime.now()
+            .subtract(new Duration(seconds: 2))
+            .millisecondsSinceEpoch,
+        postContent:
+            "This is a Contenaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaat",
         postCommentCount: 2,
         postLikeCount: 4,
         postImage: ''),
     new ThreadPostData(
-        userName: "userTest1",
+        userName: "OmOm",
         userThumbnail: "",
-        postTimeStamp: 202000,
-        postContent: "This is Content",
+        postTimeStamp: DateTime.now()
+            .subtract(new Duration(days: 7))
+            .millisecondsSinceEpoch,
+        postContent: "This is the Content",
         postCommentCount: 2,
         postLikeCount: 4,
         postImage: ''),
@@ -76,7 +84,7 @@ class _MyHomePageState extends State<MyHomePage> {
   bool _isLoading = false;
 
   void initState() {
-    _takeUserDataFromFireBase();
+    //_takeUserDataFromFireBase();
     super.initState();
   }
 
@@ -110,6 +118,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   child: Center(
                     child: CircularProgressIndicator(),
                   ),
+                  color: Colors.white.withOpacity(0.7),
                 ))
               : Container()
         ],
@@ -126,17 +135,82 @@ class _MyHomePageState extends State<MyHomePage> {
 Widget _listTile(ThreadPostData data) {
   return Padding(
     padding: const EdgeInsets.all(2.0),
-    child: Card(
-      elevation: 2.0,
-      child: ListTile(
-        onTap: () {
-          print("Tap");
-        },
-        title: Text(
-          data.postContent,
-          style: TextStyle(fontSize: 18),
-        ),
-      ),
+    child: Column(
+      children: [
+        Card(
+            elevation: 2.0,
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Icon(
+                          Icons.book,
+                          size: 34,
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            data.userName,
+                            style: TextStyle(fontSize: 18),
+                          ),
+                          Text(
+                            readTimestamp(data.postTimeStamp),
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Divider(
+                    height: 4,
+                    color: Colors.black,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(4, 10, 4, 10),
+                  ),
+                  Text(
+                    data.postContent,
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  Divider(
+                    height: 4,
+                    color: Colors.black,
+                  ),
+                  Row(
+                    children: [
+                      Row(
+                        children: [Icon(Icons.favorite_border), Text("Like")],
+                      ),
+                      Row(
+                        children: [Icon(Icons.mode_comment), Text("Comment")],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
+
+//       ListTile(
+//         onTap: () {
+//           print("Tap");
+//         },
+//         title: Text(
+//           data.postContent,
+//           style: TextStyle(fontSize: 18),
+//         ),
+//       ),
+
+            ),
+        Container(
+          height: 4,
+        )
+      ],
     ),
   );
 }
