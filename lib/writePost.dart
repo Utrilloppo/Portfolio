a7c64b5e08fe8fa0e3eccf19c330f7451c4f45c6
Urillo <hohuraman@icloud.com>
2021-07-27 12:57:17 +0900

    Regular commit

--- lib/writePost.dart
+++ lib/writePost.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:keyboard_actions/keyboard_actions.dart';
+import 'package:keyboard_actions/keyboard_actions_config.dart';
 
 class WritePost extends StatefulWidget {
   @override
@@ -7,12 +9,73 @@ class WritePost extends StatefulWidget {
 }
 
 class _WritePost extends State<WritePost> {
+  final FocusNode _nodeText1 = FocusNode();
+  TextEditingController writeTextController = TextEditingController();
+  FocusNode writingTextFocus = FocusNode();
+
+  KeyboardActionsConfig _buildConfig(BuildContext context) {
+    return KeyboardActionsConfig(
+      keyboardActionsPlatform: KeyboardActionsPlatform.ALL,
+      keyboardBarColor: Colors.grey[200],
+      nextFocus: true,
+      actions: [
+        KeyboardActionsItem(
+          displayArrows: false,
+          focusNode: _nodeText1,
+        ),
+        KeyboardActionsItem(
+          displayArrows: false,
+          focusNode: writingTextFocus,
+          toolbarButtons: [
+            (node) {
+              return GestureDetector(
+                onTap: () {
+                  print('Select Image');
+                  _getImageAndCrop();
+                },
+                child: Container(
+                  color: Colors.grey[200],
+                  padding: EdgeInsets.all(8.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.add_photo_alternate, size: 28),
+                      Text(
+                        "Add Image",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ],
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Writing Post"),
         centerTitle: true,
+        actions: [
+          FlatButton(
+              onPressed: () {
+                print("Post Start");
+              },
+              child: Text(
+                "Post",
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              )),
+        ],
       ),
       body: Text("Write Post"),
     );
